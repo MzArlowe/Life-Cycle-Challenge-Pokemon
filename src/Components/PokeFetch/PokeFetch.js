eb23c06aa3ef7a9f1241df705bc0071094c5cb27
unknown <bhm727@yahoo.com>
2022-01-12 15:37:00 -0500

    Created Timer and Stop

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 import './PokeFetch.css';
 
 
-class PokeFetch extends Component {
-  constructor() {
-    super()
+export default class PokeFetch extends Component {
+  constructor(props) {
+    super(props)
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      timer: 0,
+      timerOn: false,
+      timerInterval: 10,
+      pokemonRevealed: false
     }
   }
 
-  fetchPokemon() {
+  fetchPokemon = () => {
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -29,18 +33,52 @@ class PokeFetch extends Component {
       .catch((err) => console.log(err))
   }
 
+  //Timer that counts down from 10 seconds
+
+  startTime = () => {
+    this.fetchPokemon()
+    this.setState({ timerOn: true, pokemonRevealed: false })
+    this.setState({ timer: 10 })
+    this.setState({
+      timerInterval: setInterval(() => {
+        if (this.state.timer > 0) {
+          this.setState({
+            timer: this.state.timer - 1,
+          })
+        } else {
+          this.setState({
+            pokemonRevealed: true,
+          })
+          this.setState({
+            timerOn: false,
+          })
+          clearInterval(this.state.timerInterval)
+        }
+      }, 1000)
+    })
+  }
+
+  stopTime = () => {
+    this.setState({ timerOn: false })
+    clearInterval(this.state.timerInterval)
+  }
+
   render() {
     return (
       <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
-        <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
-        </div>
+      <button className={'start'} onClick={this.startTime}>Start</button> 
+      <h1 className={'timer'} >Timer Display</h1>
+      <div className="timerContainer">
+              <h1 className={'timer'}>{this.state.timer}</h1>
+            </div>
+      <div className={'pokeWrap'}>
+        <img className={'pokeImg'} src={this.state.pokeSprite } />
+        <h1 className={'pokeName'}>{this.state.pokeName}</h1>
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
-export default PokeFetch;
+// onClick={() => this.fetchPokemon()}>Start!</button>
+//{this.state.timerOn ? 'timerVisible' : 'timerHidden'}>{this.state.timer}</h1>
